aaa9842bcb1a26645ec200fb818341e6475b7285
Jacob Soo <jacobsoo@users.noreply.github.com>
2015-05-12 22:18:58 +0800

    Updates

---
+++ CiscoType7Decrypter/main.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace CiscoType7Decrypter
+{
+    partial class main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(main));
+            this.btnDecrypt = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.EncPass = new System.Windows.Forms.TextBox();
+            this.DecPass = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnDecrypt
+            // 
+            this.btnDecrypt.Location = new System.Drawing.Point(298, 28);
+            this.btnDecrypt.Name = "btnDecrypt";
+            this.btnDecrypt.Size = new System.Drawing.Size(75, 23);
+            this.btnDecrypt.TabIndex = 0;
+            this.btnDecrypt.Text = "Decrypt";
+            this.btnDecrypt.UseVisualStyleBackColor = true;
+            this.btnDecrypt.Click += new System.EventHandler(this.btnDecrypt_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.DecPass);
+            this.groupBox1.Controls.Add(this.EncPass);
+            this.groupBox1.Controls.Add(this.btnDecrypt);
+            this.groupBox1.Location = new System.Drawing.Point(13, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(379, 92);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Type7 Decrypt";
+            // 
+            // EncPass
+            // 
+            this.EncPass.Location = new System.Drawing.Point(113, 28);
+            this.EncPass.Name = "EncPass";
+            this.EncPass.Size = new System.Drawing.Size(175, 20);
+            this.EncPass.TabIndex = 1;
+            // 
+            // DecPass
+            // 
+            this.DecPass.Location = new System.Drawing.Point(113, 54);
+            this.DecPass.Name = "DecPass";
+            this.DecPass.ReadOnly = true;
+            this.DecPass.Size = new System.Drawing.Size(175, 20);
+            this.DecPass.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Type 7 Password :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(7, 60);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(102, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Plaintext Password :";
+            // 
+            // main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(404, 117);
+            this.Controls.Add(this.groupBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "main";
+            this.Text = "Cisco Type7 Password Decrypter";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnDecrypt;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox DecPass;
+        private System.Windows.Forms.TextBox EncPass;
+    }
+}
+
---
+++ CiscoType7Decrypter/main.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CiscoType7Decrypter
+{
+    public partial class main : Form
+    {
+        public main()
+        {
+            InitializeComponent();
+        }
+
+        private void btnDecrypt_Click(object sender, EventArgs e)
+        {
+            Type7Decryption();
+        }
+
+        public void Type7Decryption()
+        {
+            byte[] xlat = { 0x64, 0x73, 0x66, 0x64, 0x3b, 0x6b, 0x66, 0x6f, 0x41, 0x2c, 0x2e, 0x69, 0x79, 0x65, 0x77, 0x72, 0x6b, 0x6c, 0x64, 0x4a, 0x4b, 0x44, 0x48, 0x53, 0x55, 0x42 };
+            int val = 0;
+            string szEncrypted = EncPass.Text;
+            string szDecrypted = "";
+            int iUnEncrypted_length = (szEncrypted.Length - 2) / 2;
+            int iXORindex = ((HexToInt(szEncrypted[0]) - 0) * 10) + (HexToInt(szEncrypted[1]) - 0);
+
+            for (int i = 2; i < szEncrypted.Length; i = i + 2){
+                val = HexToInt(szEncrypted[i]) * 16 + HexToInt(szEncrypted[i + 1]);
+                szDecrypted += Convert.ToChar(val ^ xlat[iXORindex]);
+                iXORindex++;
+            }
+            DecPass.Text = szDecrypted;
+        }
+
+        int HexToInt(char hexChar)
+        {
+            hexChar = char.ToUpper(hexChar);  // may not be necessary
+
+            return (int)hexChar < (int)'A' ?
+                ((int)hexChar - (int)'0') :
+                10 + ((int)hexChar - (int)'A');
+        }
+    }
+}
